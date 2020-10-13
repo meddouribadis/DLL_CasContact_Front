ff8066ccc30cc4d82226ccc45123066a4f23eb59
Badis <meddouri.badis@gmail.com>
2020-10-13 14:41:38 +0200

    Conditionnal rendering

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -20,55 +20,71 @@ function HomePage() {
     }
 
     return (
-        <div className="row">
-            <div className="col-12">
-                <h1>Accueil</h1>
-                <hr/>
+        <div>
+            {user.role === 'TEACHER' &&
+            <div className="row">
+                <div className="col-12">
+                    <h1>Accueil</h1>
+                    <hr/>
 
-                <h2>Vos cas contacts :</h2>
-                <br/>
-                <div className="list-group">
-                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                        <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">Badis Meddouri</h5>
-                            <small>M2 MIAGE APP</small>
-                        </div>
-                        <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
-                        <small>Donec id elit non mi porta.</small>
-                    </a>
-                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                        <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">Anis Kassy</h5>
-                            <small>M2 MIAGE APP</small>
-                        </div>
-                        <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
-                        <small>Donec id elit non mi porta.</small>
-                    </a>
-                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                        <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">Jayson Bouceaud</h5>
-                            <small>M2 MIAGE APP</small>
-                        </div>
-                        <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
-                        <small>Donec id elit non mi porta.</small>
-                    </a>
+                    <h2>Vos cas contacts :</h2>
+                    <br/>
+                    <div className="list-group">
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">Badis Meddouri</h5>
+                                <small>M2 MIAGE APP</small>
+                            </div>
+                            <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
+                            <small>Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">Anis Kassy</h5>
+                                <small>M2 MIAGE APP</small>
+                            </div>
+                            <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
+                            <small>Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">Jayson Bouceaud</h5>
+                                <small>M2 MIAGE APP</small>
+                            </div>
+                            <p className="mb-1">Du 13/10 jusqu'au 20/10</p>
+                            <small>Donec id elit non mi porta.</small>
+                        </a>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <br/>
+                    <h2>Tableau de bord </h2>
+                    <br/>
+                </div>
+                <div className="col-4">
+                    <a className="btn btn-primary" href="#" role="button">Calendrier</a>
+                </div>
+                <div className="col-4">
+                    <a className="btn btn-primary" href="#" role="button">Gérer mes élèves</a>
+                </div>
+                <div className="col-4">
+                    <a className="btn btn-primary" href="#" role="button">Gérer mes classes</a>
                 </div>
             </div>
-            <div className="col-12">
-                <br/>
-                <h2>Tableau de bord </h2>
-                <br/>
+            }
+            {user.role === 'STUDENT' &&
+            <div className="row">
+                <div className="col-12">
+                    <h1>Accueil</h1>
+                    <hr/>
+
+                    <h2>Vos actions :</h2>
+                    <p>Bonjour étudiant</p>
+                </div>
             </div>
-            <div className="col-4">
-                <a className="btn btn-primary" href="#" role="button">Calendrier</a>
-            </div>
-            <div className="col-4">
-                <a className="btn btn-primary" href="#" role="button">Gérer mes élèves</a>
-            </div>
-            <div className="col-4">
-                <a className="btn btn-primary" href="#" role="button">Gérer mes classes</a>
-            </div>
+            }
         </div>
+
     );
 }
 
